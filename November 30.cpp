b5d6edd183cebd753c3e33fae5f9471641334b36
NiteshD1 <58332267+NiteshD1@users.noreply.github.com>
2020-01-03 08:24:41 -0500

    Create November 30.cpp
    Atcoder problem.

---
+++ November 30.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+
+
+int main(){
+    int m1,d1,m2,d2;
+    cin>>m1>>d1>>m2>>d2;
+
+    if(m2==m1+1 && d2==1){
+        cout<<1<<endl;
+
+    }else
+    cout<<0<<endl;
+
+
+
+return 0;
+}
